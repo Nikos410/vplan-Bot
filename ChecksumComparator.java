7e6f10a064cab23c2b71c9963127ca23b7295c97
Nikos410 <nikos.epping@web.de>
2016-11-07 18:06:59 +0100

    Create ChecksumComparator.java

---
+++ ChecksumComparator.java
@@ -0,0 +1,16 @@
+package org.nikos.vplanbot.org.nikos.vplanbot.checksum;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ChecksumComparator {
+    public static boolean compareChecksums(final List<byte[]> checksums) {
+        final int length = checksums.size();
+        for (int i = 0; i < length - 1; ++i) {
+            if(!Arrays.equals(checksums.get(i), checksums.get(i+1))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
