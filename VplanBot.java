fcbc16f99cef134349a0da3939d0a0312e030c04
Nikos410 <nikos.epping@web.de>
2017-01-06 22:37:36 +0100

    Timer
    Der Plan wird jetzt automatisch in regelmäßigen Abständen überprüft.

--- VplanBot.java
+++ VplanBot.java
@@ -2,8 +2,8 @@ package org.nikos.vplanbot;
 /**
  * Danke an Sebastian K. !
  *
- * Zweck dieses Bots ist es, den aktuellen Vertretungsplan herunterzuladen und zu ueberpruefen,
- * ob die Datei in den letzten 10 Minuten geaendert wurde.
+ * Zweck dieses Bots ist es, alle 10 Minuten den aktuellen Vertretungsplan herunterzuladen und zu überpruefen,
+ * ob die Datei geändert wurde.
  * <p>
  * Wichtig: es muss sich eine Datei namens "vplan.pdf" im Verzeichnis befinden!
  * </p>
@@ -11,6 +11,7 @@ package org.nikos.vplanbot;
 
 import org.nikos.vplanbot.org.nikos.vplanbot.checksum.ChecksumComparator;
 import org.nikos.vplanbot.org.nikos.vplanbot.checksum.MD5FileDigestor;
+import org.nikos.vplanbot.org.nikos.vplanbot.date.DateReader;
 import org.nikos.vplanbot.org.nikos.vplanbot.download.FileDownloader;
 import org.nikos.vplanbot.org.nikos.vplanbot.notification.GMailNotificationService;
 import org.nikos.vplanbot.org.nikos.vplanbot.notification.NotificationService;
@@ -24,62 +25,102 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.MessageFormat;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Date;
+import java.util.*;
 
 public class Vplanbot {
 
+    private Timer timer;
+    private static final int standardTimerPeriod = 600000; //600000 -> 600 Sekunden -> 10 Minuten
+    private final int timerPeriod;
+
     private String notificationMessage;
+    private String notificationSubject;
 
     private final NotificationService notificationServiceGMail;
     private final NotificationService notificationServiceTwitter;
 
-    private Vplanbot() {
+    private Vplanbot(String[] timerPeriodArgs) {
+
+        if(timerPeriodArgs.length != 0){
+            //Es gibt ein Argument
+            timerPeriod = (Integer.parseInt(timerPeriodArgs[0])*1000); //Angegebene Sekunden in die benötigten Millisekunden umwandeln
+        }
+        else {
+            //Es gibt kein Argument -> Standard verwenden
+            timerPeriod = standardTimerPeriod;
+        }
+
         try {
-            byte[] encodedSubject =  Files.readAllBytes(Paths.get("config/subject.txt"));   //Betreff auslesen
-            final String subject = new String(encodedSubject, StandardCharsets.UTF_8);
+            final byte[] encodedSubject =  Files.readAllBytes(Paths.get("config/subject.txt"));   //Betreff auslesen
+            this.notificationSubject = new String(encodedSubject, StandardCharsets.UTF_8);
 
-            byte[] encodedMessage = Files.readAllBytes(Paths.get("config/message.txt"));    //Nachricht auslesen
-            this.notificationMessage = new String(encodedMessage, StandardCharsets.UTF_8);
+            final byte[] encodedMessage = Files.readAllBytes(Paths.get("config/message.txt"));    //Nachricht auslesen
+            notificationMessage = new String(encodedMessage, StandardCharsets.UTF_8);
 
-            DateFormat dateFormat = new SimpleDateFormat("[dd.MM - HH:mm]"); //[07.11. - 17:49]
-            Date date = new Date();
-            this.notificationMessage = notificationMessage + " " + '\n' + (dateFormat.format(date));
+            final String senderMailAddress = "****@****.***";
+            final List<String> receiverMailAddresses = Collections.singletonList("****@****.***");
 
-            final String senderMailAddress = "sender@gmail.com";
-            final List<String> receiverMailAddresses = Collections.singletonList("receiver@whatever.de");
+            System.out.println( "\u001B[36m" + "Config:" + "\u001B[0m" + '\n' +
+                                "\u001B[36m" + "Timer Periodendauer: " + "\u001B[0m" + timerPeriod/1000 + " Sekunden" + '\n' +
+                                "\u001B[36m" + "Betreff: " + "\u001B[0m" + this.notificationSubject + '\n' +
+                                "\u001B[36m" + "Nachricht: " + "\u001B[0m" + this.notificationMessage + '\n' +
+                                "\u001B[36m" + "Absender Mail-Adresse: " + "\u001B[0m" + senderMailAddress + '\n' +
+                                "\u001B[36m" + "Empfänger Mail-Adresse(n): " + "\u001B[0m" + receiverMailAddresses + '\n');
 
             this.notificationServiceGMail = new GMailNotificationService(
                     senderMailAddress,
                     receiverMailAddresses,
-                    subject,
                     Paths.get("vplan.pdf"));
 
             this.notificationServiceTwitter = new TwitterNotificationService();
 
         } catch (IOException e) {
-            throw new RuntimeException("Could not read subject and/or message files.", e);
+            throw new RuntimeException("Could not readArea subject and/or message files.", e);
         }
+
+        //Timer erstellen
+        timer = new Timer();
     }
 
     private void run() {
+
+        this.checkChange();
+
+        //Timer erinrichten und starten
+        timer.scheduleAtFixedRate(new Task(), timerPeriod, timerPeriod);
+
+        try {
+            while (true) {
+                Thread.sleep(1000);
+            }
+        }
+        catch(InterruptedException e){
+            System.err.println("InterruptedException: " + e.getLocalizedMessage());
+        }
+    }
+
+    private class Task extends TimerTask {
+        @Override public void run(){
+            checkChange();
+        }
+    }
+
+    private void checkChange(){
+        System.out.println('\n' + "Vertretungsplan wird überprüft! " + DateReader.date("[dd.MM. - HH:mm:ss]"));
+
         final Path existingFile = Paths.get("vplan.pdf");
-        final Path newFile = downloadFile();
+        final Path newFile = downloadFile("website.de/plan/vplan.pdf");
 
         final List<byte[]> checksums = MD5FileDigestor.digestFiles(Arrays.asList(existingFile, newFile));
 
         if (ChecksumComparator.compareChecksums(checksums)) {
             //Beide Dateien sind gleich -> Der Plan nicht veraendert.
-            System.out.println('\n' + "\u001B[33m" + "Keine Aenderung gefunden!");
+            System.out.println("\u001B[33m" + "Keine Aenderung gefunden!" + "\u001B[0m");
 
             //Heruntergeladenen Plan loeschen, da er nicht anders ist
             try {
                 Files.delete(newFile);
-                System.out.println("Temporaere Datei geloescht.");
+                System.out.println("\u001B[33m" + "Temporaere Datei geloescht." + "\u001B[0m");
             } catch (IOException e) {
                 final String errorMessage = MessageFormat.format("Could not delete file {0}. Reason: {1}", newFile, e.getLocalizedMessage());
                 System.err.println(errorMessage);
@@ -87,15 +128,18 @@ public class Vplanbot {
             }
         } else {
             //Die Dateien sind unterschiedlich -> Der Plan wurde veraendert
-            System.out.println('\n' + "\u001B[36m" + "Aenderung gefunden!");
+            System.out.println("\u001B[36m" + "Aenderung gefunden!" + "\u001B[0m");
             replaceFile(existingFile, newFile);
 
-            //Mail Benachrichtigung
-            notificationServiceGMail.sendNotification(this.notificationMessage);
-            System.out.println('\n' + "\u001B[36m" + "E-Mail Benachrichtigung verschickt.");
+            final String dateNotificationMessage = notificationMessage + " " + '\n' + DateReader.date("[dd.MM. - HH:mm]");
 
-            notificationServiceTwitter.sendNotification(this.notificationMessage);
-            System.out.println('\n' + "\u001B[36m" + "Twitter Benachrichtigung verschickt.");
+            //Twitter Benachrichtung (Standard Text, für alle)
+            notificationServiceTwitter.sendNotification(dateNotificationMessage);
+            System.out.println('\n' + "\u001B[36m" + "Twitter Benachrichtigung verschickt." + "\u001B[0m");
+
+            //Mail Benachrichtigung
+            notificationServiceGMail.sendNotification(dateNotificationMessage, this.notificationSubject);
+            System.out.println('\n' + "\u001B[36m" + "E-Mail Benachrichtigung verschickt." + "\u001B[0m");
         }
     }
 
@@ -112,8 +156,8 @@ public class Vplanbot {
         System.out.println("\u001B[34m" + "vplan.pdf" + "\u001B[36m" + " aktualisiert." + "\u001B[0m" + '\n');
     }
 
-    private Path downloadFile() {
-        final String fileToDownload = "URL to vplan.pdf";
+    private Path downloadFile(String url) {
+        final String fileToDownload = url;
         try {
             final URL fileUrl = new URL(fileToDownload);
             final FileDownloader downloader = new FileDownloader(fileUrl, Paths.get("vplanTEMP.pdf"));
@@ -126,6 +170,7 @@ public class Vplanbot {
     }
 
     public static void main(String[] args) {
-        new Vplanbot().run();
+        //Argumente: Timer-Periodendauer in Sekunden
+        new Vplanbot(args).run();
     }
 }
